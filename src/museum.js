666c8d69673af63ac9f21c780583cce62cb65c57
Huong <calm.and.chaotic@gmail.com>
2023-07-04 11:56:06 +0200

    Update with light, camera and a test cube

--- src/museum.js
+++ src/museum.js
@@ -8,71 +8,97 @@ init();
 render();
 
 function init() {
+  /**
+   * CONTAINER
+   */
   const container = document.getElementById("container");
+  const modelContainer = document.createElement("div");
+  modelContainer.style.width = "500px";
+  modelContainer.style.height = "300px";
+  container.appendChild(modelContainer);
 
-  // Scene
+  /**
+   * SCENE
+   */
   scene = new THREE.Scene();
 
-  // Camera
-  camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.25,
-    20
-  );
-  camera.position.set(1, 1, 15);
+  // Set the background color
+  scene.background = new THREE.Color("skyblue");
 
-  // Render
-  renderer = new THREE.WebGLRenderer({ antialias: true });
+  /**
+   * CAMERA
+   */
+  // Create a camera
+  const fov = 35; // AKA Field of View
+  const aspect = modelContainer.clientWidth / modelContainer.clientHeight;
+  const near = 0.1; // the near clipping plane
+  const far = 100; // the far clipping plane
 
-  // Set default background color
-  renderer.setClearColor(0xffffff, 1.0);
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.toneMapping = THREE.ACESFilmicToneMapping;
-  renderer.toneMappingExposure = 1;
-  renderer.useLegacyLights = false;
+  // every object is initially created at ( 0, 0, 0 )
+  // move the camera back so we can view the scene
+  camera.position.set(0, 0, 10);
 
+  /**
+   * LIGHT
+   */
+  const mainLight = new THREE.DirectionalLight("white", 4);
+  mainLight.position.set(10, 10, 10);
+  scene.add(mainLight);
+
+  const light = new THREE.AmbientLight(0x404040); // soft white light
+  scene.add(light);
+
+  /**
+   * MODEL
+   */
   const loader = new GLTFLoader();
-
-  // Model
-  // modelInfo.forEach((model) => {
-  // Create container for model
-  const modelContainer = document.createElement("div");
-  container.appendChild(modelContainer);
 
   // Load model
   loader.load(
     // "/assets/models/" + model.path + "/scene.gltf",
     "/assets/models/perseus_fighting_medusa/scene.gltf",
     function (gltf) {
+      // gltf.scene.scale.set(0.001, 0.001, 0.001);
+
+      // Add model to the scene
       scene.add(gltf.scene);
+
       render();
     },
-    undefined,
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    },
     function (error) {
       console.error(error);
     }
   );
+
+  // Test for something other than model
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  /**
+   * RENDER
+   */
+  renderer = new THREE.WebGLRenderer();
+
+  // Set the renderer to the same size as our container element
+  renderer.setSize(500, 300);
+
+  // Set the pixel ratio so that our scene will look good on HiDPI displays
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  // Turn on the physically correct lighting model
+  renderer.useLegacyLights = true;
+
+  // Add the automatically created <canvas> element to the page
   modelContainer.appendChild(renderer.domElement);
-  // create a cube and add to scene
-  // var cubeGeometry = new THREE.BoxGeometry(
-  //   10 * Math.random(),
-  //   10 * Math.random(),
-  //   10 * Math.random()
-  // );
 
-  // var cubeMaterial = new THREE.MeshNormalMaterial();
-
-  // var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  // scene.add(cube);
-
-  // Add rendered model to container
-
-  render();
-
-  // });
+  // render();
 
   window.addEventListener("resize", onWindowResize);
 }
@@ -86,6 +112,7 @@ function onWindowResize() {
   render();
 }
 
+// Render, or 'create a still image', of the scene
 function render() {
   renderer.render(scene, camera);
 }
